a386bbcde7a51cce7704fc5ff0769d5b7c82e35f
zz1qwop <84325395+zz1qwop@users.noreply.github.com>
2023-06-22 18:29:16 +0900

    Feat: 검색 기능 (더미 데이터 사용)

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LuNewspaper } from 'react-icons/lu';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    navigate(`search/${search}`, { state: { search } });
+  };
   return (
     <div className="flex w-full mt-8 mb-14">
       <Link
@@ -13,15 +20,17 @@ export default function Header() {
         <LuNewspaper className="mr-2" />
         <p>NEWS</p>
       </Link>
-      <div className="flex w-full justify-center">
+      <form className="flex w-full justify-center" onSubmit={handleForm}>
         <input
           type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           className="bg-white border border-slate-300 rounded-md text-sm w-9/12 mr-4 px-4 focus:outline-none focus:border-gray-500 focus:ring-1 focus:ring-gray-500"
         />
-        <button>
+        <button type="submit">
           <AiOutlineSearch className="text-2xl text-gray-700" />
         </button>
-      </div>
+      </form>
     </div>
   );
 }
--- src/Components/NewsList.jsx
+++ src/Components/NewsList.jsx
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import NewsItem from './NewsItem';
 import { v4 as uuidv4 } from 'uuid';
+import { useParams } from 'react-router-dom';
 
 export default function NewsList() {
   const [data, setData] = useState();
+  const { search } = useParams();
 
   useEffect(() => {
     const loadData = async () => {
-      const response = await axios.get('/data/all_category.json');
+      let response;
+      if (search) {
+        response = await axios.get('/data/search.json');
+      } else {
+        response = await axios.get('/data/all_category.json');
+      }
       setData(response.data.articles);
     };
 
     loadData();
-  }, []);
+  }, [search]);
 
   return (
     <div className="w-5/6 grid grid-cols-3 gap-4">
